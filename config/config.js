e9a6f115668f427b26e53defde0236ab15dd5ec7
Brandon LaRouche <bjlarouche@verizon.net>
2019-04-02 21:13:34 -0400

    Adding config.js

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// slack.js: config for DevRel-Node-Service app modules
+// Should be treated as immutable
+
+// Author(s): Brandon LaRouche
+
+var authStash = JSON.parse(require('fs').readFileSync(__dirname + '/../config/auth_stash.js', "utf8"));
+
+var config = {
+
+}
+
+module.exports = config;
