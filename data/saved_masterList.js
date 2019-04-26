647d2a2d6350a9c8c9c2608e0614ea1a40a6fa10
Brandon LaRouche <bjlarouche@verizon.net>
2019-04-26 12:07:19 -0400

    Include saved_masterList.js

---
+++ data/saved_masterList.js
@@ -0,0 +1,86 @@
+{
+   "MCAS": {
+      "AADS": [],
+      "ARTH": [],
+      "BIOL": [],
+      "UNCP": [],
+      "CHEM": [],
+      "CLAS": [],
+      "COMM": [],
+      "CSCI": [],
+      "UNCS": [],
+      "EESC": [],
+      "EALC": [],
+      "ECON": [],
+      "ENGL": [],
+      "ENVS": [],
+      "FILM": [],
+      "FREN": [],
+      "HIST": [],
+      "HONR": [],
+      "INTL": [],
+      "ICSP": [],
+      "ITAL": [],
+      "JESU": [],
+      "JOUR": [],
+      "LING": [],
+      "MATH": [],
+      "ROTC": [],
+      "MUSA": [],
+      "MUSP": [],
+      "NELC": [],
+      "PHIL": [],
+      "PHYS": [],
+      "POLI": [],
+      "PSYC": [],
+      "RLRL": [],
+      "SLAV": [],
+      "SOCY": [],
+      "SPAN": [],
+      "ARTS": [],
+      "THTR": [],
+      "THEO": [],
+      "UNAS": []
+   },
+   "CSOM": {
+      "ACCT": [],
+      "BSLW": [],
+      "MCOM": [],
+      "MFIN": [],
+      "ISYS": [],
+      "GSOM": [],
+      "MHON": [],
+      "MPRX": [],
+      "MGMT": [],
+      "MKTG": [],
+      "OPER": [],
+      "PRTO": [],
+      "UGMG": []
+   },
+   "CSON": {
+      "FORS": [],
+      "NURS": [],
+      "HLTH": []
+   },
+   "LAW": {
+      "LAWS": []
+   },
+   "LYNCH": {
+      "APSY": [],
+      "ELHE": [],
+      "ERME": [],
+      "EDUC": []
+   },
+   "SSW": {
+      "SCWK": []
+   },
+   "STM": {
+      "TMCE": [],
+      "TMHW": [],
+      "TMNT": [],
+      "TMOT": [],
+      "TMPS": [],
+      "TMRE": [],
+      "TMST": []
+   }
+}
